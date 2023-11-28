1bbe3e3f4c9af4dbc24db46c65379ad46afc53e3
Michał Kapuściński <alert251c@gmail.com>
2023-11-28 21:03:51 +0100

    fully implemented

--- app/src/main/java/com/michalkapuscinski/christmascard/MainActivity.kt
+++ app/src/main/java/com/michalkapuscinski/christmascard/MainActivity.kt
@@ -1,9 +1,19 @@
 package com.michalkapuscinski.christmascard
 
+import android.Manifest
+import android.content.ContentResolver
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.icu.util.Calendar
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.michalkapuscinski.christmascard.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,4 +31,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun Context.resourceUri(resourceId: Int): Uri = with(resources) {
+        Uri.Builder()
+            .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+            .authority(getResourcePackageName(resourceId))
+            .appendPath(getResourceTypeName(resourceId))
+            .appendPath(getResourceEntryName(resourceId))
+            .build()
+    }
+
+    private fun initTempUri(): Uri {
+        val tempImagesDir = File(
+            applicationContext.filesDir, //this function gets the external cache dir
+            getString(R.string.temp_images_dir)) //gets the directory for the temporary images dir
+        tempImagesDir.mkdir()
+        val tempImage = File(
+            tempImagesDir, //prefix the new abstract path with the temporary images dir path
+            getString(R.string.temp_image)) //gets the abstract temp_image file name
+        return FileProvider.getUriForFile(
+            applicationContext,
+            getString(R.string.authorities),
+            tempImage)
+    }
 }
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Kotlin basics:
+https://kotlinlang.org/docs/basic-syntax.html
+
+Material:
+https://m3.material.io/
+
+Material github:
+https://github.com/material-components/material-components-android/tree/master/docs/components
